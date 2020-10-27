44f90c3db3808a5d1d86d4d1608ad4ea510d1c6b
User <uzairbangee@gmail.com>
2020-10-27 13:30:10 +0500

    feat: adding webconfig

--- gatsby-node.js
+++ gatsby-node.js
@@ -23,3 +23,18 @@ exports.createPages = async ({ graphql, actions }) => {
     })
   })
 }
+
+exports.onCreateWebpackConfig = ({actions, stage}) => {
+  if (stage === "build-html") {
+      actions.setWebpackConfig({
+          module: {
+              rules: [
+                  {
+                      test: /firebase/,
+                      use: ['null-loader']
+                  },
+              ],
+          }
+      })
+  }
+};
